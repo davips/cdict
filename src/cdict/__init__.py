014441ce80c993c9c286c56075be17ee193f2622
davips <dpsabc@gmail.com>
2021-12-26 03:15:28 -0300

    Implement full i-values

---
+++ src/cdict/__init__.py
@@ -0,0 +1,1 @@
+from cdict.cd import cd
---
+++ src/cdict/value/lazyival.py
@@ -0,0 +1,101 @@
+#  Copyright (c) 2021. Davi Pereira dos Santos
+#  This file is part of the cdict project.
+#  Please respect the license - more about this in the section (*) below.
+#
+#  cdict is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  cdict is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with cdict.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  (*) Removing authorship by any means, e.g. by distribution of derived
+#  works or verbatim, obfuscated, compiled or rewritten versions of any
+#  part of this work is illegal and it is unethical regarding the effort and
+#  time spent here.
+#
+import operator
+from functools import reduce
+from typing import Union
+
+from hosh import Hosh
+
+from cdict.identification import f2hosh
+from cdict.value.absival import AbsiVal
+
+
+class LazyiVal(AbsiVal):
+    """
+    Identified lazy value
+
+    Threefold lazy: It is calculated only when needed, only once and it is cached.
+
+    >>> cache = {}
+    >>> from cdict.value.ival import iVal
+    >>> deps = {"x": iVal(2)}
+    >>> lvx = LazyiVal(lambda x: x**2, 0, 1, deps, [], cache=cache)
+    >>> lvx
+    →(x)
+    >>> deps = {"x": lvx, "y": iVal(3)}
+    >>> result = []
+    >>> f = lambda x,y: [x+y, y**2]
+    >>> lvy = LazyiVal(f, 0, 2, deps, result, cache=cache)
+    >>> lvz = LazyiVal(f, 1, 2, deps, result, cache=cache)
+    >>> lvx, lvy, lvz
+    (→(x), →(x→(x) y), →(x→(x) y))
+    >>> deps = {"z":lvz}
+    >>> f = lambda z: {"z":z**3, "w":z**5}
+    >>> result = []
+    >>> lvz2 = LazyiVal(f, 0, 2, deps, result, cache=cache)
+    >>> lvw = LazyiVal(f, 1, 2, deps, result, cache=cache)
+    >>> lvx, lvy, lvz2, lvw
+    (→(x), →(x→(x) y), →(z→(x→(x) y)), →(z→(x→(x) y)))
+    >>> lvx.v, lvy.v, lvz2.v, lvw.v
+    (4, 7, 729, 59049)
+    >>> lvz2.id
+    'hWbiHpbI2eCTvRg7AAOU8Inraf2k7pajWDj46aJA'
+    >>> lvw.id
+    'O1ZMWqMUI-2N6HISHWxNH7hFluQrhHkCQX79ckq9'
+    >>> lvz2.h + lvw.h == lvz.h * lvw.fh
+    True
+    """
+
+    def __init__(self, f: callable, i: int, n: int, deps: dict, result: list, fid: Union[str, Hosh] = None, cache=None):
+        self.f = f
+        self.i = i
+        self.n = n
+        self.deps = {} if deps is None else deps
+        self.result = result
+        self.fh = f2hosh(f) if fid is None else self.handle_id(fid)
+        self.cache = {} if cache is None else cache
+        self.h = (reduce(operator.mul, self.deps.values()) * self.fh).component(i, n)
+
+    @property
+    def v(self):
+        if not self.result:
+            deps = {}
+            for field, ival in self.deps.items():
+                deps[field] = ival.v
+            result = self.f(**deps)
+            if self.n == 1:
+                result = [result]
+            elif isinstance(result, dict):
+                result = result.values()
+            elif isinstance(result, list) and len(result) != self.n:  # pragma: no cover
+                raise Exception(f"Wrong result length: {len(result)} differs from {self.n}")
+            try:
+                self.result.extend(result)
+            except TypeError:  # pragma: no cover
+                raise Exception(f"Unsupported multi-valued result type: {type(result)}")
+        return self.result[self.i]
+
+    def __repr__(self):
+        if not self.result:
+            return f"→({' '.join(k + ('' if dep.result else str(dep)) for k, dep in self.deps.items())})"
+        return repr(self.v)
